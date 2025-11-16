76408932dcfeaf5835b668dd4ca231af285572e7
Julia Zahorovska <ju.zagorovsky@gmail.com>
2025-11-16 23:58:53 +0100

    add readme, change toml, setup, data save and load paths

--- src/personal_assistant/pickle_data.py
+++ src/personal_assistant/pickle_data.py
@@ -1,18 +1,34 @@
+import os
+import pickle
 from .addressbook import AddressBook
 from .notes import Notes
-import pickle
+
+DEFAULT_DIR = os.path.join(os.path.expanduser("~"), ".assistant")
+DEFAULT_FILE = os.path.join(DEFAULT_DIR, "addressbook.pkl")
 
 
-def save_data(book, notes, filename="addressbook.pkl"):
-    with open(filename, "wb") as f:
-        data = {"address_book": book, "notes": notes}
-        pickle.dump(data, f)
+# Creates directory ~/.assistant if it doesn't exist.
+def ensure_data_dir():
+    if not os.path.exists(DEFAULT_DIR):
+        os.makedirs(DEFAULT_DIR)
 
 
-def load_data(filename="addressbook.pkl"):
-    try:
-        with open(filename, "rb") as f:
-            data = pickle.load(f)
-            return data.get("address_book", AddressBook()), data.get("notes", Notes())
-    except FileNotFoundError:
+# Loads data from ~/.assistant/addressbook.pkl or creates new.
+def load_data(filepath=DEFAULT_FILE):
+    ensure_data_dir()
+
+    if os.path.exists(filepath) and os.path.getsize(filepath) > 0:
+        with open(filepath, "rb") as file:
+            data = pickle.load(file)
+            return data.get("addressbook", AddressBook()), data.get("notes", Notes())
+    else:
         return AddressBook(), Notes()
+
+
+# Saves data to ~/.assistant/addressbook.pkl.
+def save_data(addressbook, notes, filepath=DEFAULT_FILE):
+    ensure_data_dir()
+
+    data = {"addressbook": addressbook, "notes": notes}
+    with open(filepath, "wb") as file:
+        pickle.dump(data, file)
